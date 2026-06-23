39f1031cf61c81fd2ac4ce859d278b8794a815db
aayushrajputz <aayushpratapsingh001@gmail.com>
2026-06-22 23:13:30 -0700

    OOPS

--- oop/abstraction/index.ts
+++ oop/abstraction/index.ts
@@ -1,1 +1,45 @@
-// Write your Abstraction code here step-by-step with the Coach
+class badCoffeeMachine {
+    public grindBeans() {
+        console.log("Grinding coffee beans...")
+    }
+    public brewCoffee() {
+        console.log("brewing fresh coffee");
+
+    }
+    public boilWater() {
+        console.log("boiling water... ");
+
+    }
+}
+
+const machine = new badCoffeeMachine();
+machine.boilWater();
+machine.brewCoffee();
+machine.grindBeans();
+
+
+class goodCoffeeMachine {
+    private grindBeans() {
+        console.log("grinding coffee beans..");
+
+
+    }
+    private brewCoffee() {
+        console.log("brewing coffee");
+
+    }
+    private boilWater() {
+        console.log("boiling water");
+
+    }
+    public makeCoffee() {
+        this.grindBeans();
+        this.boilWater();
+        this.brewCoffee();
+        console.log("here is your perfect coffee");
+
+    }
+}
+
+const goodMachine = new goodCoffeeMachine();
+goodMachine.makeCoffee(); 
--- oop/inheritance/index.ts
+++ oop/inheritance/index.ts
@@ -1,1 +1,80 @@
-// Write your Inheritance code here step-by-step with the Coach
+class badCar {
+    make: string
+    model: string
+    honk: "meow" | "beep"
+    constructor(make: string, model: string, honk: "meow" | "beep") {
+        this.make = make
+        this.model = model
+        this.honk = honk
+    }
+    openTruck(): void {
+        console.log("truck open");
+
+    }
+
+}
+
+class badTruck {
+    make: string
+    model: string
+    honk: "meow" | "beep"
+    constructor(make: string, model: string, honk: "meow" | "beep") {
+        this.make = make
+        this.model = model
+        this.honk = honk
+    }
+    loadCargo(): void {
+        console.log("cargo load");
+
+    }
+
+}
+
+const startEngine = (vechile: badCar | badTruck) => {
+    console.log("engine is starting");
+
+}
+
+class Vechile {
+    protected make: string
+    protected model: string
+    protected honk: "meow" | "beep"
+    protected door: number
+    constructor(make: string, model: string, honk: "meow" | "beep", door: number) {
+        this.make = make
+        this.model = model
+        this.honk = honk
+        this.door = door
+    }
+    startEngine(): void {
+        console.log("engine started for vechile");
+
+    }
+}
+class car extends Vechile {
+    constructor(make: string, model: string, honk: "meow" | "beep", door: number) {
+        super(make, model, honk, door)
+    }
+    openTruck(): void {
+        console.log("truck open");
+
+    }
+}
+
+class truck extends Vechile {
+    constructor(make: string, model: string, honk: "meow" | "beep", door: number) {
+
+        super(make, model, honk, door)
+    }
+    loadCargo(): void {
+        console.log("cargo load");
+    }
+}
+
+const myCar = new car("Honda", "Civic", "beep", 4);
+
+// 1. startEngine() humne 'car' class ke andar nahi likha tha, par hum ise call kar sakte hain kyunki it extends Vechile!
+myCar.startEngine();
+
+// 2. Custom car method ko call karo
+myCar.openTruck();
