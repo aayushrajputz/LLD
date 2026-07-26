2fa79e669dba9cf9744dd505eaf3cf331a333f98
aayushrajputz <aayushpratapsingh001@gmail.com>
2026-07-26 12:47:21 +0530

    ElevatorSystem

---
+++ Case Studies/ElevatorSystem/Request.ts
@@ -0,0 +1,25 @@
+import { Direction } from "./Enums.ts"
+
+export class Request {
+    private sourceFloor: number;
+    private destinationFloor: number;
+    private direction: Direction;
+
+    constructor(sourceFloor: number, destinationFloor: number, direction: Direction) {
+        this.destinationFloor = destinationFloor;
+        this.sourceFloor = sourceFloor;
+        this.direction = direction;
+    }
+
+    public getSourceFloor(): number {
+        return this.sourceFloor;
+    }
+
+    public getDestinationFloor(): number {
+        return this.destinationFloor;
+    }
+
+    public getDirection(): Direction {
+        return this.direction;
+    }
+} 
